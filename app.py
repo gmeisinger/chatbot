dfd4908d471c6bd18f3fe8b5590de3e05d68071b
Ubuntu <ubuntu@ip-172-31-29-18.us-east-2.compute.internal>
2020-09-28 20:43:32 +0000

    working on POST

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, url_for
 
 application = Flask(__name__, static_folder='static')
 
 @application.route("/")
 def index():
-    return "hello"
-    #return send_from_directory(app.static_folder, 'index.html')
+    #return "hello"
+    return send_from_directory(application.static_folder, 'index.html')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')

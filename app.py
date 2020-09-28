f520af7b9dc20a349c7d97ad6a90b5b8ab4f29e8
gmeisinger <gmeisinger@gmail.com>
2020-09-28 16:07:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ application = Flask(__name__, static_folder='static')
 
 @application.route("/")
 def index():
-    return send_from_directory(app.static_folder, 'index.html')
+    return "hello"
+    #return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')

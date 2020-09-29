f8632cfc8802f0d33035df16af5e3b70cb3b3352
gmeisinger <gmeisinger@gmail.com>
2020-09-28 20:20:33 -0400

    chat history displays

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
-from flask import Flask, send_from_directory, url_for, render_template, request
+from flask import Flask, send_from_directory, url_for, render_template, request, session
 
 application = Flask(__name__, static_folder='templates/static')
 
-chat_history = []
 username = "User"
+session["chat_history"] = []
 
 @application.route("/")
 def index():
-    #return "hello"
-    return render_template("index.html")
+    return render_template("index.html", chat=session["chat_history"])
 
 @application.route("/submit", methods=["GET", "POST"])
 def submit():
     if request.method == "POST":
         msg = [username, request.form["usermsg"]]
-        chat_history.append(msg)
-        return render_template("index.html", chat=chat_history)
-    else:
-        return render_template("index.html")
+        session["chat_history"].append(msg)
+    return render_template("index.html", chat=session["chat_history"])
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')

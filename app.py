225bd50cac29fdb20190346a13aa76b13418bbc1
gmeisinger <gmeisinger@gmail.com>
2020-09-28 21:00:07 -0400

    using sessions

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, send_from_directory, url_for, render_template, request,
 application = Flask(__name__, static_folder='templates/static')
 application.config['SECRET_KEY'] = "its a secret!"
 
+Session(application)
+
 chat_history = []
 username = "User"
 
@@ -11,26 +13,23 @@ def generate_response(msg=None):
     text = "What?"
     return [author, text]
 
-@application.before_request
-def before_request():
-    if "chat_history" not in session:
-        session["chat_history"] = []
-
 @application.route("/")
 def index():
+    if session["chat"] is None:
+        session["chat"] = []
     #return render_template("index.html", chat=session["chat_history"])
-    return render_template("index.html", chat=chat_history)
+    return render_template("index.html", chat=session["chat"])
 
 @application.route("/submit", methods=["GET", "POST"])
 def submit():
     if request.method == "POST":
         msg = [username, request.form["usermsg"]]
         #session["chat_history"].append(msg)
-        chat_history.append(msg)
+        session["chat"].append(msg)
         response = generate_response(msg)
-        chat_history.append(response)
+        session["chat"].append(response)
     #return render_template("index.html", chat=session["chat_history"])
-    return render_template("index.html", chat=chat_history)
+    return render_template("index.html", chat=session["chat"])
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')

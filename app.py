30e68b1dee6a497c6528f37d164b05a41dc367b2
gmeisinger <gmeisinger@gmail.com>
2020-10-03 16:16:40 -0400

    used a random generator as example to get socketio working

--- app.py
+++ app.py
@@ -1,43 +1,75 @@
 from flask import Flask, send_from_directory, url_for, render_template, request, session
-from flask_session import Session
+#from flask_session import Session
+from flask_socketio import SocketIO, emit
+
+from random import random
+from time import sleep
+from threading import Thread, Event
+
 
 application = Flask(__name__, static_folder='templates/static')
-application.config['SESSION_TYPE'] = 'filesystem'
-application.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+application.config['DEBUG'] = True
+application.config['SECRET_KEY'] = 'secret!'
 
-Session(application)
+#Session(application)
 
-chat_history = []
-username = "User"
+#chat_history = []
+#username = "User"
 
+socketio = SocketIO(application, async_mode=None, logger=True, engineio_logger=True)
+
+#random number Generator Thread
+thread = Thread()
+thread_stop_event = Event()
+
+def randomNumberGenerator():
+    """
+    Generate a random number every 1 second and emit to a socketio instance (broadcast)
+    Ideally to be run in a separate thread?
+    """
+    #infinite loop of magical random numbers
+    print("Making random numbers")
+    while not thread_stop_event.isSet():
+        number = round(random()*10, 3)
+        print(number)
+        socketio.emit('newnumber', {'number': number}, namespace='/test')
+        socketio.sleep(5)
+"""
 def generate_response(msg):
     author = "Chatbot"
     text = hello(msg)
     return [author, text]
 
-@application.route("/")
-def index():
-    if "chat" not in session:
-        session["chat"] = []
-    #return render_template("index.html", chat=session["chat_history"])
-    return render_template("index.html", chat=session["chat"])
-
-@application.route("/submit", methods=["GET", "POST"])
-def submit():
-    if request.method == "POST":
-        msg = [username, request.form["usermsg"]]
-        #session["chat_history"].append(msg)
-        session["chat"].append(msg)
-        response = generate_response(msg)
-        session["chat"].append(response)
-    #return render_template("index.html", chat=session["chat_history"])
-    return render_template("index.html", chat=session["chat"])
-	
 def hello(msg):
   if "hello" in msg:
     return "Hello, how can I help you?"
   else:
     return "What?"
+"""
+@application.route("/")
+def index():
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+"""
+@socketio.on('message')
+def receive_message(message):
+    print("Got a message!", flush=True)
+    return "Echo: %s"%(message,)
+"""
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    # need visibility of the global thread object
+    global thread
+    print('Client connected')
+
+    #Start the random number generator thread only if the thread has not been started before.
+    if not thread.isAlive():
+        print("Starting Thread")
+        thread = socketio.start_background_task(randomNumberGenerator)
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+if __name__ == '__main__':
+    socketio.run(app)
--- templates/static/index.js
+++ templates/static/index.js
@@ -1,62 +1,22 @@
-var storage = window.sessionStorage;
-var num_entries = storage.getItem('num_entries');
-var entries_string = storage.getItem('entries');
-var entries = [];
 
-if (entries_string === null) {
-    entries = [];
-}
-else {
-    entries = JSON.parse(entries_string);
-}
-if (num_entries === null) {
-    num_entries = 0;
-}
+$(document).ready(function(){
+    //connect to the socket server.
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+    var numbers_received = [];
 
-var i;
-for (i = 0; i < entries.length; i++) {
-    $("#chatbox").append(entries[i]);
-}
-
-$(function () {
-    var user_text_element = document.getElementById("usermsg");
-    user_text_element.focus()
-
-    console.log(storage.getItem('entries'));
-
-    //when enter is pressed
-    $("#usermsg").on('keydown', function(e) {
-        // e.preventDefault();
-        if (e.which == 13) {
-            console.log('Enter pressed!');
-            document.getElementById("submitmsg").click();
-            return false;
+    //receive details from server
+    socket.on('newnumber', function(msg) {
+        console.log("Received number" + msg.number);
+        //maintain a list of ten numbers
+        if (numbers_received.length >= 10){
+            numbers_received.shift()
+        }            
+        numbers_received.push(msg.number);
+        numbers_string = '';
+        for (var i = 0; i < numbers_received.length; i++){
+            numbers_string = numbers_string + '<p>' + numbers_received[i].toString() + '</p>';
         }
+        $('#log').html(numbers_string);
     });
-
-    var add_text = function (entered_text) {
-        var new_entry = '<div class="row"> <div class="col"><b>' + 'user' + ': </b>' + entered_text + '</div> </div>';
-        num_entries++;
-        storage.setItem('num_entries', num_entries);
-        entries.push(new_entry);
-        storage.setItem('entries', JSON.stringify(entries));
-        $("#chatbox").append(new_entry)
-        $("#usermsg").val("")
-        //var objDiv = document.getElementById("wrapper");
-        // objDiv.scrollTop = objDiv.scrollHeight;
-    }
-
-    $("#submitmsg").on('click', function(e) {
-        console.log("submitmsg clicked");
-        var entered_text = $("#usermsg").val()
-        add_text(entered_text)
-    })
-
-    //$.post( "/postmethod", {
-    //    canvas_data: JSON.stringify(outputData)
-    //}, function(err, req, resp){
-    //    window.location.href = "/results/"+resp["responseJSON"]["uuid"];  
-    //  });
-
 })
 

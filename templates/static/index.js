706d01bab36395965863f20dac7c3693ce742d75
gmeisinger <gmeisinger@gmail.com>
2020-10-03 17:09:54 -0400

    testing connect/disconnect

--- templates/static/index.js
+++ templates/static/index.js
@@ -5,6 +5,21 @@ $(document).ready(function(){
     var numbers_received = [];
 
     var chatbox = document.getElementById("chatbox");
+    var user_input = document.getElementById("usermsg");
+
+    user_input.focus();
+
+    // adding elements to chat
+    var add_text = function (uname, entered_text) {
+        $("#chatbox").append(
+            '<div class="row"> <div class="col"><b>' + uname + ': </b>' + entered_text + '</div> </div>')
+        $("#usermsg").val("")
+        chat.scrollTop = chat.scrollHeight;
+    }
+
+    socket.on('connect', function () {
+        console.log('Socket Connection Establised!')
+    });
 
     //receive details from server
     socket.on('newnumber', function(msg) {

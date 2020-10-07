74fce15e5d366f505358262d63df985606f6943b
gmeisinger <gmeisinger@gmail.com>
2020-10-07 19:27:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -99,6 +99,7 @@ def get_case_history(country, case_type="confirmed", start_date=None, end_date=N
         if end_date != None and type(end_date) is str:
             r_string += "&to=" + start_date + midnight
     print("r-string: " + r_string, flush=True)
+    print("https://api.covid19api.com/total/country/united-states/status/deaths?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z" == r_string, flush=True)
     r = req.get(r_string)
     data = r.json()
     return data

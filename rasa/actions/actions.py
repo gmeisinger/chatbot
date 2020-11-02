2f28883c6b37b5e8964ad70e54eaa8c05a717c60
Jacob Zomper <jacob.zomper@gmail.com>
2020-11-02 18:58:21 -0500

    first attempt at two country case count

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -85,3 +85,80 @@ class ActionCaseCount(Action):
         )
         #return [slot_scope, slot_case_type, slot_country, slot_count, evt]
         return []
+
+class ActionCaseCountMultCountry(Action):
+
+    @staticmethod
+    def required_fields():
+        return [
+            #EntityFormField("scope", "scope"),
+            #EntityFormField("case_type", "case_type"),
+            EntityFormField("country", "country")
+        ]
+
+    def name(self):
+        return "action_case_count_multiple_country"
+    
+    def run(self, dispatcher, tracker, domain):
+        # get data
+        scope = tracker.get_slot('scope')
+        if scope == None:
+            scope = "total"
+        case_type = tracker.get_slot('case_type')
+        if case_type == None:
+            case_type = "confirmed"
+        country = tracker.get_slot('countries')
+        # get data from api
+        r = requests.get("https://api.covid19api.com/summary")
+        summary = r.json()
+        # target a country
+        countries = summary['Countries']
+        if country == None:
+            data = summary['Global']
+            key_string = scope.capitalize() + case_type.capitalize()
+            count = data[key_string]
+            # report the information
+            slot_scope = SlotSet(key='scope', value=scope)
+            slot_case_type = SlotSet(key='case_type', value=case_type)
+            slot_country = SlotSet(key='country', value=country)
+            slot_count = SlotSet(key='count', value=count)
+            evt = FollowupAction(name = "utter_case_count")
+            dispatcher.utter_message(
+                template="utter_case_count",
+                count=count,
+                scope=scope,
+                case_type=case_type,
+                country=country
+            )
+        else:
+            print("country:")
+            for x in country:
+                print(x + ",\n")
+            if len(country) == 2: #try with just 2 to make sure it can work
+                data1 = next((item for item in countries if (item['Slug'] == country[0] or item['Country'] == country[0])), None)
+                key_string1 = scope.capitalize() + case_type.capitalize()
+                count1 = data1[key_string1]
+                data2 = next((item for item in countries if (item['Slug'] == country[1] or item['Country'] == country[1])), None)
+                key_string2 = scope.capitalize() + case_type.capitalize()
+                count2 = data2[key_string1]
+                # report the information
+                slot_scope = SlotSet(key='scope', value=scope)
+                slot_case_type = SlotSet(key='case_type', value=case_type)
+                slot_country = SlotSet(key='country1', value=country[0])
+                slot_country = SlotSet(key='country2', value=country[1])
+                slot_count = SlotSet(key='count1', value=count1)
+                slot_count = SlotSet(key='count2', value=count2)
+                evt = FollowupAction(name = "utter_case_count_two_country")
+                dispatcher.utter_message(
+                    template="utter_case_count_two_country",
+                    count1=count1,
+                    count2=count2,
+                    scope=scope,
+                    case_type=case_type,
+                    country1=country[0],
+                    country2=country[1]
+                )
+            else:
+                dispatcher.utter_message(text="Well why didnt this work?")
+        #return [slot_scope, slot_case_type, slot_country, slot_count, evt]
+        return []

82ea0d073a3d457edcc2c03ac72ebd8157e3e520
gmeisinger <gmeisinger@gmail.com>
2020-11-14 14:15:06 -0500

    Update actions.py

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -243,7 +243,7 @@ class ActionCaseSummaryGraph(Action):
         linechart = self.Linechart(title, dayone, vtag, ltag)
         # write linechart out to temporary file
         jspath = 'tmp/graph.png'
-        path = '../../templates/static/tmp/graph.png'
+        path = '/home/ubuntu/chatbot/templates/static/tmp/graph.png'
         #if (os.path.exists(path)):
         #    os.remove(path)
         f = open(path, 'xb')

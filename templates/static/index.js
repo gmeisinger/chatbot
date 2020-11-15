3ed5ff70cfe35b8e7559a94d26b187ce62a22934
gmeisinger <gmeisinger@gmail.com>
2020-11-15 10:58:41 -0500

    Update index.js

--- templates/static/index.js
+++ templates/static/index.js
@@ -1,9 +1,3 @@
-$this->output->set_header("Expires: Tue, 01 Jan 2000 00:00:00 GMT"); 
-$this->output->set_header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
-$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0"); 
-$this->output->set_header("Cache-Control: post-check=0, pre-check=0", false); 
-$this->output->set_header("Pragma: no-cache");
-
 var username = 'user';
 
 // feedback stuff
